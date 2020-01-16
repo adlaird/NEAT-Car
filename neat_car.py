8e699a9d448a70ccf64baba13dfc200f7ab07239
Austin <austin.laird07@gmail.com>
2020-01-16 00:43:17 -0500

    create population and reporters

--- neat_car.py
+++ neat_car.py
@@ -17,7 +17,15 @@ def run(config_file):
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
                          config_file)
+
+    pop = neat.Population(config)
     
+    pop.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    pop.add_reporter(stats)
+
+    # winner = p.run(eval_genomes, 50)
+
     X_POSITION = 50
     Y_POSITION = 50
     RADIUS = 10
